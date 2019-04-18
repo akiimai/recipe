39df31a9b8a05613697bc456a1cb16623b4323c7
Aki Imai <akimai808@gmail.com>
2019-04-18 09:01:36 -0700

    Updates

---
+++ precache-manifest.880b691ad968b2fc4138b027a1458cd9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9079fb3b0ed48b601118",
+    "url": "/recipe/static/js/runtime~main.9079fb3b.js"
+  },
+  {
+    "revision": "c29d1da7e46751719911",
+    "url": "/recipe/static/js/main.c29d1da7.chunk.js"
+  },
+  {
+    "revision": "d1f7816cb9b9f9e640b7",
+    "url": "/recipe/static/js/2.d1f7816c.chunk.js"
+  },
+  {
+    "revision": "c29d1da7e46751719911",
+    "url": "/recipe/static/css/main.e98cddbb.chunk.css"
+  },
+  {
+    "revision": "d1f7816cb9b9f9e640b7",
+    "url": "/recipe/static/css/2.741524f0.chunk.css"
+  },
+  {
+    "revision": "89552d1d999ce903f2b969c23120c0c7",
+    "url": "/recipe/index.html"
+  }
+];
